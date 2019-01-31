55b657b5c21a18ce3c9a43d2498c239df7613d27
Mr zh <877854886@qq.com>
2019-01-31 11:30:41 +0800

    fix

--- pages/case/case.js
+++ pages/case/case.js
@@ -1,130 +1,128 @@
-import {Api} from '../../utils/api.js';
+import { Api } from '../../utils/api.js';
 var api = new Api();
 const app = getApp();
 
 Page({
-  data: {
-   currentId:0,
-   searchItem:{
-   },
-   mainData:[],
-   isFirstLoadAllStandard:['getMainData'],
 
-  },
+    data: {
+        currentId: 0,
+        searchItem: {},
+        mainData: [],
+        isFirstLoadAllStandard: ['getMainData'],
 
-  onLoad(options){
-    const self = this;
-    var nowTime = new Date().getTime();
-    if(options.id&&options.id==1){
-      self.data.currentId = options.id;
-      self.data.searchItem.open_time = ['>',nowTime]
-    }else if(options.id&&options.id==2){
-      self.data.searchItem.open_time = ['<',nowTime]
-    }
-    api.commonInit(self);
-    self.setData({
-      web_currentId:self.data.currentId
-    });
-    self.getMainData();
-  },
+    },
 
-  getMainData(isNew){
-    const  self =this;
-    if(isNew){
-      api.clearPageIndex(self)
-    };
-    const postData={};
-    postData.tokenFuncName = 'getProjectToken';
-    postData.paginate = api.cloneForm(self.data.paginate);
-    postData.searchItem = api.cloneForm(self.data.searchItem);
-    postData.searchItem.thirdapp_id = getApp().globalData.thirdapp_id;
-    postData.order = {
-      open_time:'desc'
-    };
-    const callback =(res)=>{
-      if(res.info.data.length>0){
-        self.data.mainData.push.apply(self.data.mainData,res.info.data);
-      }else{
-        self.data.isLoadAll = true;
-        api.showToast('没有更多了','fail');
-      };
-      api.buttonCanClick(self,true);
-      api.checkLoadAll(self.data.isFirstLoadAllStandard,'getMainData',self);
-      self.setData({
-        web_mainData:self.data.mainData,
-      });
-    };
-    api.messageGet(postData,callback);
-  },
+    onLoad(options) {
+        const self = this;
+        var nowTime = new Date().getTime();
+        if (options.id && options.id == 1) {
+            self.data.currentId = options.id;
+            self.data.searchItem.open_time = ['>', nowTime]
+        } else if (options.id && options.id == 2) {
+            self.data.searchItem.open_time = ['<', nowTime]
+        }
+        api.commonInit(self);
+        self.setData({
+            web_currentId: self.data.currentId
+        });
+        self.getMainData();
+    },
 
-  messageDelete(e){
-    const  self =this;
-    var index = api.getDataSet(e,'index');
-    const postData={};
-    postData.tokenFuncName = 'getProjectToken';
-    postData.searchItem={
-      id:self.data.mainData[index].id
-    };
-    postData.data= {
-      status:-1
-    };
-    const callback =(res)=>{
-      if(res.solely_code==100000){
-        api.showToast('删除成功','none')
-      }else{
-        api.showToast(res.msg,'none')
-      };
-      self.getMainData(true)
-    };
-    api.messageDelete(postData,callback);
-  },
+    getMainData(isNew) {
+        const self = this;
+        if (isNew) {
+            api.clearPageIndex(self)
+        };
+        const postData = {};
+        postData.tokenFuncName = 'getProjectToken';
+        postData.paginate = api.cloneForm(self.data.paginate);
+        postData.searchItem = api.cloneForm(self.data.searchItem);
+        postData.searchItem.thirdapp_id = getApp().globalData.thirdapp_id;
+        postData.order = {
+            open_time: 'desc'
+        };
+        const callback = (res) => {
+            if (res.info.data.length > 0) {
+                self.data.mainData.push.apply(self.data.mainData, res.info.data);
+            } else {
+                self.data.isLoadAll = true;
+                api.showToast('没有更多了', 'fail');
+            };
+            api.buttonCanClick(self, true);
+            api.checkLoadAll(self.data.isFirstLoadAllStandard, 'getMainData', self);
+            self.setData({
+                web_mainData: self.data.mainData,
+            });
+        };
+        api.messageGet(postData, callback);
+    },
 
-
-
-  changeSearch(e){
-    const self = this;
-    api.buttonCanClick(self);
-    self.data.searchItem = {};
-    var nowTime = new Date().getTime();
-    var currentId = api.getDataSet(e,'id');
-    if(currentId==1){
-      self.data.searchItem.open_time = ['>',nowTime]
-    }else if(currentId==2){
-      self.data.searchItem.open_time = ['<',nowTime]
-    }
-    self.setData({
-      web_currentId:currentId
-    });
-    self.getMainData(true);
-  },
-
-  onReachBottom() {
-    const self = this;
-    if(!self.data.isLoadAll&&self.data.buttonCanClick){
-      self.data.paginate.currentPage++; 
-      self.getMainData();
-    };
-  },
+    messageDelete(e) {
+        const self = this;
+        var index = api.getDataSet(e, 'index');
+        const postData = {};
+        postData.tokenFuncName = 'getProjectToken';
+        postData.searchItem = {
+            id: self.data.mainData[index].id
+        };
+        postData.data = {
+            status: -1
+        };
+        const callback = (res) => {
+            if (res.solely_code == 100000) {
+                api.showToast('删除成功', 'none')
+            } else {
+                api.showToast(res.msg, 'none')
+            };
+            self.getMainData(true)
+        };
+        api.messageDelete(postData, callback);
+    },
 
 
 
-  intoPath(e){
-    const self = this;
-    api.pathTo(api.getDataSet(e,'path'),'nav');
-  },
+    changeSearch(e) {
+        const self = this;
+        api.buttonCanClick(self);
+        self.data.searchItem = {};
+        var nowTime = new Date().getTime();
+        var currentId = api.getDataSet(e, 'id');
+        if (currentId == 1) {
+            self.data.searchItem.open_time = ['>', nowTime]
+        } else if (currentId == 2) {
+            self.data.searchItem.open_time = ['<', nowTime]
+        }
+        self.setData({
+            web_currentId: currentId
+        });
+        self.getMainData(true);
+    },
 
-  tab(e){
-    this.setData({
-      currentId:e.currentTarget.dataset.id
-    })
-  },
+    onReachBottom() {
+        const self = this;
+        if (!self.data.isLoadAll && self.data.buttonCanClick) {
+            self.data.paginate.currentPage++;
+            self.getMainData();
+        };
+    },
 
 
-  intoPathRedirect(e){
-    const self = this;
-    api.pathTo(api.getDataSet(e,'path'),'redi');
-  }, 
- 
+
+    intoPath(e) {
+        const self = this;
+        api.pathTo(api.getDataSet(e, 'path'), 'nav');
+    },
+
+    tab(e) {
+        this.setData({
+            currentId: e.currentTarget.dataset.id
+        })
+    },
+
+
+    intoPathRedirect(e) {
+        const self = this;
+        api.pathTo(api.getDataSet(e, 'path'), 'redi');
+    },
+
 })
-
-  
--- pages/caseDetail/caseDetail.js
+++ pages/caseDetail/caseDetail.js
@@ -1,54 +1,56 @@
-import {Api} from '../../utils/api.js';
+import { Api } from '../../utils/api.js';
 var api = new Api();
 const app = getApp();
-import {Token} from '../../utils/token.js';
+import { Token } from '../../utils/token.js';
 const token = new Token();
 
 Page({
-  data: {
- 
-   isFirstLoadAllStandard:['getMainData'],
-  },
+    data: {
 
-  onLoad(options){
-    const self = this;
-    api.commonInit(self);
-    self.data.id= options.id;
-    self.getMainData();
-  },
+        isFirstLoadAllStandard: ['getMainData'],
+    },
 
-  getMainData(){
-    const  self =this;
-    const postData={};
-    postData.tokenFuncName = 'getProjectToken';
-    postData.searchItem = {
-      thirdapp_id:getApp().globalData.thirdapp_id,
-      id:self.data.id
-    };
-    const callback =(res)=>{
-      if(res.info.data.length>0){
-        self.data.mainData = res.info.data[0];
-      }else{
-        api.showToast('数据错误','none');
-      };
-      api.checkLoadAll(self.data.isFirstLoadAllStandard,'getMainData',self);
-      self.setData({
-        web_mainData:self.data.mainData,
-      });
-    };
-    api.messageGet(postData,callback);
-  },
+    onLoad(options) {
+        const self = this;
+        api.commonInit(self);
+        self.data.id = options.id;
+        self.getMainData();
+    },
+
+    getMainData() {
+        const self = this;
+        const postData = {};
+        postData.tokenFuncName = 'getProjectToken';
+        postData.searchItem = {
+            thirdapp_id: getApp().globalData.thirdapp_id,
+            id: self.data.id
+        };
+        const callback = (res) => {
+            if (res.info.data.length > 0) {
+                self.data.mainData = res.info.data[0];
+            } else {
+                api.showToast('数据错误', 'none');
+            };
+            api.checkLoadAll(self.data.isFirstLoadAllStandard, 'getMainData', self);
+            self.setData({
+                web_mainData: self.data.mainData,
+            });
+        };
+        api.messageGet(postData, callback);
+    },
 
 
-  
 
-  intoPath(e){
-    const self = this;
-    api.pathTo(api.getDataSet(e,'path'),'nav');
-  },
 
- 
+    intoPath(e) {
+        const self = this;
+        api.pathTo(api.getDataSet(e, 'path'), 'nav');
+    },
+
+    intoPathRedirect(e) {
+        const self = this;
+        api.pathTo(api.getDataSet(e, 'path'), 'redi');
+    },
+
 
 })
-
-  
